b855f60ff698da0f098f8f17348fa2cd052a2785
githubsagarkumar <kumarsagar3142@gmail.com>
2024-11-24 18:33:53 +0530

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,219 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import blog1 from '../assets/images/blog/blog-1.jpg'
+import blog2 from '../assets/images/blog/blog-2.jpg'
+import blog3 from '../assets/images/blog/blog-3.jpg'
+import blog4 from '../assets/images/blog/blog-4.jpg'
+import a6 from '../assets/images/about/a6.jpg'
+import video from '../assets/images/about/1.mp4'
+import Marquee from "react-fast-marquee";
+import orders from '../assets/images/icons/icon1.png'
+import orders1 from '../assets/images/icons/icon2.png'
+import orders2 from '../assets/images/icons/icon3.png'
+import orders3 from '../assets/images/icons/icon4.png'
+import orders4 from '../assets/images/icons/icon5.png'
+
+import mobileImage from '../assets/images/mobileimage.jpg'
+const about = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check the screen width on component mount and when it's resized
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Call the handleResize function initially
+    handleResize();
+
+    // Add an event listener to handle screen resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return <>
+    <section className="about-wrapper p-5 d-flex justify-content-center align-items-center">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="shop-details text-center align-items-center">
+              <h1 className="text-white">#Know Us</h1>
+              <p className='text-white fs-3'>Get to know us more</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="about-us p-5">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={a6} alt="" className='img-fluid p-5' />
+        </div>
+        <div className="col-md-6 d-flex flex-column align-items-center justify-content-center">
+          <h1><b>Who are we?</b></h1>
+          <p className="card-text mb-3"> <br /> 
+          Pradeep Readymade is your ultimate destination for stylish, comfortable, and affordable clothing. Whether you're looking for casual wear, formal attire, or trendy outfits for special occasions, we have something for everyone. Our wide range of collections caters to men, women, and kids, ensuring the perfect fit for every family member. At Pradeep Readymade, we believe in delivering quality apparel at unbeatable prices, making fashion accessible to all. Step in today and discover the latest trends, vibrant designs, and exceptional craftsmanship that redefine your wardrobe without breaking the bank. Dress to impress with Pradeep Readymade – where style meets affordability! <br /></p>
+
+        </div>
+      </div>
+    </section>
+
+    <section className="about-app p-5">
+      <h2 className='fs-1 text-center mb-4' >Download our <Link>App</Link></h2>
+      <div className="col-12 p-5 col-md-8 mx-auto">
+        {isMobile ? (
+          // Render a mobile-friendly video or image here
+          <img src={mobileImage} alt="Mobile Image" className='w-100' />
+        ) : (
+          // Render the video for larger screens
+          <video loop muted autoPlay src={video} className='w-100'></video>
+        )}
+      </div>
+    </section>
+
+    <section className="abouts p-5">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12 d-flex flex-column align-items-center">
+            <h1 className='mb-3'>What our customers say...</h1>
+            <p className='mb-4'>Our customers never miss a bit on providing feedback</p>
+          </div>
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog1} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Marvel Clein</h5>
+                    <p className="card-text mb-3">"Pradeep Readymade offers a fantastic variety of clothing. Whether I need casual wear or formal outfits, I always find something that suits my style." <br />..</p>
+                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog2} alt="" className='img-fluid rounded-start' />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Melisa Ivy</h5>
+                    <p className="card-text mb-3">"The pricing is very reasonable, making it easy to shop for the whole family without overspending. Great value for money!" <br />.</p>
+                    <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog3} alt="" className='img-fluid rounded-start' />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Tristian Ann</h5>
+                    <p className="card-text mb-3">"The quality of the clothes is excellent. They are durable, comfortable, and look stylish no matter the occasion." <br />.</p>
+                    <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog4} alt="" className='img-fluid rounded-start' />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Phillip Omosh</h5>
+                    <p className="card-text mb-3">"The staff is extremely polite and helpful. They assist with selecting outfits and ensure you leave with a smile."<br /></p>
+                    <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog2} alt="" className='img-fluid rounded-start' />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Melisa Ivy</h5>
+                    <p className="card-text mb-3">"The store always keeps up with the latest fashion trends. I love how stylish and modern their collection is!" <br />.</p>
+                    <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 p-3">
+            <div className="card mb-3">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={blog1} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Marvel Clein</h5>
+                    <p className="card-text mb-3">"Pradeep Readymade caters to men, women, and children, making it a one-stop shop for my entire family’s clothing needs."<br /></p>
+                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 p-3">
+            <Marquee className='p-2 slide'>
+              <div>
+                <img src={orders} alt="" className='img-fluid' />
+                <p className="card-text mb-2">Fast Orders</p>
+              </div>
+              <div>
+                <img src={orders1} alt="" className='img-fluid' />
+                <p className="card-text mb-2">Quick Shipping</p>
+
+              </div>
+              <div>
+                <img src={orders2} alt="" className='img-fluid' />
+                <p className="card-text mb-2">High Saves</p>
+              </div>
+              <div>
+                <img src={orders3} alt="" className='img-fluid' />
+                <p className="card-text">24/7 Support</p>
+              </div>
+              <div>
+                <img src={orders4} alt="" className='img-fluid' />
+                <p className="card-text">Online Orders</p>
+              </div>
+            </Marquee>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>;
+}
+
+export default about
---
+++ src/pages/blog.jsx
@@ -0,0 +1,122 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import b1 from '../assets/images/blog/b1.jpg'
+import b6 from '../assets/images/blog/b6.jpg'
+import b2 from '../assets/images/blog/b2.jpg'
+import b3 from '../assets/images/blog/b3.jpg'
+import b4 from '../assets/images/blog/b4.jpg'
+import b5 from '../assets/images/blog/b5.jpg'
+import b7 from '../assets/images/blog/b7.jpg'
+
+const blog = () => {
+  return <>
+  <section className="blog-wrapper p-5">
+    <div className="container-xxl">
+      <div className="row">
+      <div className="col-12">
+          <div className="shop-details text-center align-items-center">
+            <h1 className="text-white">#Read More</h1>
+            <p className='text-white fs-3'>Get to know what our trusted customers say...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section className="blogs p-5">
+    <div className="container-xxl container">
+      <div className="row">
+        <div className="d-flex flex-column align-items-center">
+          <h1 className='mb-3'>Explore more in our library</h1>
+          <p className='mb-4'>Why we have trending Outfits Everywhere</p>
+        </div>
+        <div className="col-12 p-5">
+          <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={b1} className="img-fluid rounded-start" alt="..." />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">Marvel Clein</h5>
+                <p className="card-text">"Pradeep Readymade offers a fantastic variety of clothing. Whether I need casual wear or formal outfits, I always find something that suits my style." <br /></p>
+                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              </div>
+              </div>
+           
+             </div>
+          </div>
+        </div>
+        <div className="col-12 p-5">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={b2} alt="" className='img-fluid rounded-start' />
+                  </div>
+                  <div className="col-md-6">
+                    <div className="card-body">
+                      <h5 className="card-title">Melisa Ivy</h5>
+                      <p className="card-text">"The pricing is very reasonable, making it easy to shop for the whole family without overspending. Great value for money!"<br /></p>
+                      <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 p-5">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={b3} alt="" className='img-fluid rounded-start' />
+                  </div>
+                  <div className="col-md-6">
+                    <div className="card-body">
+                      <h5 className="card-title">Tristian Ann</h5>
+                      <p className="card-text">"I’ve been a regular customer for years, and Pradeep Readymade has never disappointed me with its service and quality."<br /></p>
+                      <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 p-5">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={b4} alt="" className='img-fluid rounded-start' />
+                  </div>
+                  <div className="col-md-6">
+                    <div className="card-body">
+                      <h5 className="card-title">Phillip Omosh</h5>
+                      <p className="card-text">"Pradeep Readymade offers an excellent range of clothing at reasonable prices. I found everything I needed for my family in just one visit!"<br /></p>
+                      <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 p-5">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={b2} alt="" className='img-fluid rounded-start' />
+                  </div>
+                  <div className="col-md-6">
+                    <div className="card-body">
+                      <h5 className="card-title">Melisa Ivy</h5>
+                      <p className="card-text">"Pradeep Readymade combines style with affordability. Their quality and pricing keep me coming back!"<br /></p>
+                      <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+      </div>
+    </div>
+  </section>
+  </>;
+}
+
+export default blog
